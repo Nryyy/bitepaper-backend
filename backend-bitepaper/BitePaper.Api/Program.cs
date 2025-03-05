975cc78af32a2da53b101c17cbd0ab1d3bf44a98
Nazarii Soboliuk <nazarii.soboliuk@oa.edu.ua>
2025-03-06 00:59:56 +0200

    Edit Program.cs

--- backend-bitepaper/BitePaper.Api/Program.cs
+++ backend-bitepaper/BitePaper.Api/Program.cs
@@ -1,9 +1,4 @@
-using BitePaper.Application.Handlers.Departments;
-using BitePaper.Infrastructure.Interfaces.Departments;
 using FastEndpoints;
-using BitePaper.Infrastructure.Repositories.Departments;
-using BitePaper.Infrastructure.Services.Departments;
-using MediatR;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,18 +6,6 @@ builder.Services.AddOpenApi();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddFastEndpoints();
-
-builder.Services.AddScoped<IDepartmentRepository, DepartmentRepository>();
-builder.Services.AddScoped<IDepartmentService, DepartmentService>();
-
-builder.Services.AddMediatR(configuration =>
-{
-    configuration.RegisterServicesFromAssembly(typeof(CreateDepartmentHandler).Assembly);
-    configuration.RegisterServicesFromAssembly(typeof(UpdateDepartmentHandler).Assembly);
-    configuration.RegisterServicesFromAssembly(typeof(DeleteDepartmentHandler).Assembly);
-    configuration.RegisterServicesFromAssembly(typeof(GetAllDepartmentHandler).Assembly);
-    configuration.RegisterServicesFromAssembly(typeof(GetDepartmentByIdHandler).Assembly);
-});
 
 var app = builder.Build();
 
