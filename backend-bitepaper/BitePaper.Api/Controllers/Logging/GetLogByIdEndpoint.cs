33510b269f696d525d2c44349c2b0d5161287fce
Nazarii Soboliuk <109824446+Nryyy@users.noreply.github.com>
2025-04-28 21:19:50 +0300

    Update GetLogByIdEndpoint.cs
    new namespace

--- backend-bitepaper/BitePaper.Api/Controllers/Logging/GetLogByIdEndpoint.cs
+++ backend-bitepaper/BitePaper.Api/Controllers/Logging/GetLogByIdEndpoint.cs
@@ -3,20 +3,18 @@ using BitePaper.Models.DTOs.Request.Logs;
 using FastEndpoints;
 using MediatR;
 
-namespace BitePaper.Api.Controllers.Logs
+namespace BitePaper.Api.Controllers.Logs;
+
+public class GetLogByIdEndpoint(IMediator mediator) : Endpoint<DeleteLogRequest>
 {
-    public class GetLogByIdEndpoint(IMediator mediator) : Endpoint<DeleteLogRequest>
+    public override void Configure()
     {
-        public override void Configure()
-        {
-            Get("log/{id}");
-            AllowAnonymous();
-        }
-        public override async Task HandleAsync(DeleteLogRequest request, CancellationToken ct)
-        {
-            var logs = await mediator.Send(new GetLogByIdQuery(request.Id), ct);
-            await SendAsync(logs, cancellation: ct);
-        }
+        Get("log/{id}");
+        AllowAnonymous();
+    }
+    public override async Task HandleAsync(DeleteLogRequest request, CancellationToken ct)
+    {
+        var logs = await mediator.Send(new GetLogByIdQuery(request.Id), ct);
+        await SendAsync(logs, cancellation: ct);
     }
 }
-
