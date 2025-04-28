32e6e0b792df60cf57f7c9e88a94ab63b7baa15c
Nazarii Soboliuk <109824446+Nryyy@users.noreply.github.com>
2025-04-28 21:37:27 +0300

    Update ILogRepository.cs
    new namespace

--- backend-bitepaper/BitePaper.Infrastructure/Interfaces/Logging/ILogRepository.cs
+++ backend-bitepaper/BitePaper.Infrastructure/Interfaces/Logging/ILogRepository.cs
@@ -1,14 +1,12 @@
 ﻿using BitePaper.Models.Entities;
 
-namespace BitePaper.Infrastructure.Interfaces.Logs
-{
-    public interface ILogRepository
-    {
-        Task<List<Log>> GetAllAsync();
-        Task<Log?> GetByIdAsync(string id);
-        Task CreateAsync(Log log);
-        Task UpdateAsync(Log log);
-        Task DeleteAsync(string id);
+namespace BitePaper.Infrastructure.Interfaces.Logs;
 
-    }
+public interface ILogRepository
+{
+    Task<List<Log>> GetAllAsync();
+    Task<Log?> GetByIdAsync(string id);
+    Task CreateAsync(Log log);
+    Task UpdateAsync(Log log);
+    Task DeleteAsync(string id);
 }
