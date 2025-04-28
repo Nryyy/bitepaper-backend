48c45341655a5f7b46d5ce8cbdbc6887b2d8b2ed
Nazarii Soboliuk <109824446+Nryyy@users.noreply.github.com>
2025-04-28 21:21:23 +0300

    Update DeleteSignatureEndpoint.cs
    new namespace

--- backend-bitepaper/BitePaper.Api/Controllers/Signatures/DeleteSignatureEndpoint.cs
+++ backend-bitepaper/BitePaper.Api/Controllers/Signatures/DeleteSignatureEndpoint.cs
@@ -3,19 +3,18 @@ using BitePaper.Models.DTOs.Request.Signatures;
 using FastEndpoints;
 using MediatR;
 
-namespace BitePaper.Api.Controllers.Signatures
+namespace BitePaper.Api.Controllers.Signatures;
+
+public class DeleteSignatureEndpoint(IMediator mediator) : Endpoint<DeleteSignatureRequest>
 {
-    public class DeleteSignatureEndpoint(IMediator mediator) : Endpoint<DeleteSignatureRequest>
+    public override void Configure()
     {
-        public override void Configure()
-        {
-            Delete("signature/delete/{id}");
-            AllowAnonymous();
-        }
-        public override async Task HandleAsync(DeleteSignatureRequest request, CancellationToken cancellationToken)
-        {
-            await mediator.Send(new DeleteSignatureCommand(request.Id), cancellationToken);
-            await SendAsync("Delete succeed!", cancellation: cancellationToken);
-        }
+        Delete("signature/delete/{id}");
+        AllowAnonymous();
+    }
+    public override async Task HandleAsync(DeleteSignatureRequest request, CancellationToken cancellationToken)
+    {
+        await mediator.Send(new DeleteSignatureCommand(request.Id), cancellationToken);
+        await SendAsync("Delete succeed!", cancellation: cancellationToken);
     }
 }
