54a3b773e0483abf19d5fe67bd26487e7968c75c
Nazarii Soboliuk <109824446+Nryyy@users.noreply.github.com>
2025-04-28 21:23:30 +0300

    Update CreateLogCommand.cs
    new namespace

--- backend-bitepaper/BitePaper.Application/Commands/Logging/CreateLogCommand.cs
+++ backend-bitepaper/BitePaper.Application/Commands/Logging/CreateLogCommand.cs
@@ -1,7 +1,6 @@
 ﻿using BitePaper.Models.DTOs.Request.Logs;
 using MediatR;
 
-namespace BitePaper.Application.Commands.Logs
-{
-    public record CreateLogCommand(CreateLogRequest Request) : IRequest;
-}
+namespace BitePaper.Application.Commands.Logs;
+
+public record CreateLogCommand(CreateLogRequest Request) : IRequest;
