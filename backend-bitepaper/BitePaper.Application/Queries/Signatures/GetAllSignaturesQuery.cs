3e31f1bb4ea3a23087c17bedab16ba34787969b0
Nazarii Soboliuk <109824446+Nryyy@users.noreply.github.com>
2025-04-28 21:34:18 +0300

    Update GetAllSignaturesQuery.cs
    new namespace

--- backend-bitepaper/BitePaper.Application/Queries/Signatures/GetAllSignaturesQuery.cs
+++ backend-bitepaper/BitePaper.Application/Queries/Signatures/GetAllSignaturesQuery.cs
@@ -1,7 +1,6 @@
 ﻿using BitePaper.Models.Entities;
 using MediatR;
 
-namespace BitePaper.Application.Queries.Signatures
-{
-    public record GetAllSignaturesQuery : IRequest<List<Signature>>;
-}
+namespace BitePaper.Application.Queries.Signatures;
+
+public record GetAllSignaturesQuery : IRequest<List<Signature>>;
