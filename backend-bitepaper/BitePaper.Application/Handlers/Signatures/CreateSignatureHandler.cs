f1784fae931400d1d3f4818d96904d8c2de88373
Nazarii Soboliuk <109824446+Nryyy@users.noreply.github.com>
2025-04-28 21:28:31 +0300

    Update CreateSignatureHandler.cs
    new namespace

--- backend-bitepaper/BitePaper.Application/Handlers/Signatures/CreateSignatureHandler.cs
+++ backend-bitepaper/BitePaper.Application/Handlers/Signatures/CreateSignatureHandler.cs
@@ -3,27 +3,26 @@ using BitePaper.Infrastructure.Interfaces.Signatures;
 using BitePaper.Models.Entities;
 using MediatR;
 
-namespace BitePaper.Application.Handlers.Signatures
+namespace BitePaper.Application.Handlers.Signatures;
+
+public class CreateSignatureHandler : IRequestHandler<CreateSignatureCommand>
 {
-    public class CreateSignatureHandler : IRequestHandler<CreateSignatureCommand>
+    private readonly ISignatureService _signatureService;
+
+    public CreateSignatureHandler(ISignatureService signatureService)
     {
-        private readonly ISignatureService _signatureService;
+        _signatureService = signatureService;
+    }
 
-        public CreateSignatureHandler(ISignatureService signatureService)
+    public async Task Handle(CreateSignatureCommand request, CancellationToken cancellationToken)
+    {
+        var signature = new Signature
         {
-            _signatureService = signatureService;
-        }
+            DocumentId = request.Request.DocumentId,
+            UserId = request.Request.UserId,
+            SignatureData = request.Request.SignatureData
+        };
 
-        public async Task Handle(CreateSignatureCommand request, CancellationToken cancellationToken)
-        {
-            var signature = new Signature
-            {
-                DocumentId = request.Request.DocumentId,
-                UserId = request.Request.UserId,
-                SignatureData = request.Request.SignatureData
-            };
-
-            await _signatureService.CreateAsync(signature);
-        }
+        await _signatureService.CreateAsync(signature);
     }
 }
