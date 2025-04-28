25c49e5a72811b99fb26a403bf986a3922d2fa65
Nazarii Soboliuk <109824446+Nryyy@users.noreply.github.com>
2025-04-28 21:35:41 +0300

    Update IDocumentRepository.cs
    new namespace

--- backend-bitepaper/BitePaper.Infrastructure/Interfaces/Documents/IDocumentRepository.cs
+++ backend-bitepaper/BitePaper.Infrastructure/Interfaces/Documents/IDocumentRepository.cs
@@ -1,13 +1,12 @@
 ﻿using BitePaper.Models.Entities;
 
-namespace BitePaper.Infrastructure.Interfaces.Documents
+namespace BitePaper.Infrastructure.Interfaces.Documents;
+
+public interface IDocumentRepository
 {
-    public interface IDocumentRepository
-    {
-        Task<List<Document>> GetAllAsync();
-        Task<Document?> GetByIdAsync(string id);
-        Task CreateAsync(Document document);
-        Task UpdateAsync(Document document);
-        Task DeleteAsync(string id);
-    }
+    Task<List<Document>> GetAllAsync();
+    Task<Document?> GetByIdAsync(string id);
+    Task CreateAsync(Document document);
+    Task UpdateAsync(Document document);
+    Task DeleteAsync(string id);
 }
