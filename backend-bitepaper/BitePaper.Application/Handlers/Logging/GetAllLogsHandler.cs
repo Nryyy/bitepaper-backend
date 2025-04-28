fc52808b747975ecfe613a970c7b9ce83c60320f
Nazarii Soboliuk <109824446+Nryyy@users.noreply.github.com>
2025-04-28 21:27:12 +0300

    Update GetAllLogsHandler.cs
    new namespace

--- backend-bitepaper/BitePaper.Application/Handlers/Logging/GetAllLogsHandler.cs
+++ backend-bitepaper/BitePaper.Application/Handlers/Logging/GetAllLogsHandler.cs
@@ -3,18 +3,17 @@ using BitePaper.Infrastructure.Interfaces.Logs;
 using BitePaper.Models.Entities;
 using MediatR;
 
-namespace BitePaper.Application.Handlers.Logs
+namespace BitePaper.Application.Handlers.Logs;
+
+public class GetAllLogsHandler : IRequestHandler<GetAllLogQuery, List<Log>>
 {
-    public class GetAllLogsHandler : IRequestHandler<GetAllLogQuery, List<Log>>
+    private readonly ILogRepository _logRepository;
+    public GetAllLogsHandler(ILogRepository logRepository)
     {
-        private readonly ILogRepository _logRepository;
-        public GetAllLogsHandler(ILogRepository logRepository)
-        {
-            _logRepository = logRepository;
-        }
-        public async Task<List<Log>> Handle(GetAllLogQuery request, CancellationToken cancellationToken)
-        {
-            return await _logRepository.GetAllAsync();
-        }
+        _logRepository = logRepository;
+    }
+    public async Task<List<Log>> Handle(GetAllLogQuery request, CancellationToken cancellationToken)
+    {
+        return await _logRepository.GetAllAsync();
     }
 }
