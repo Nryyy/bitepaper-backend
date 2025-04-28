5660b64929a60da87e7bd0b597af0079cc02272a
Nazarii Soboliuk <109824446+Nryyy@users.noreply.github.com>
2025-04-28 21:17:30 +0300

    Update CreateLogEndpoint.cs
    new namespace

--- backend-bitepaper/BitePaper.Api/Controllers/Logging/CreateLogEndpoint.cs
+++ backend-bitepaper/BitePaper.Api/Controllers/Logging/CreateLogEndpoint.cs
@@ -3,20 +3,19 @@ using BitePaper.Models.DTOs.Request.Logs;
 using FastEndpoints;
 using MediatR;
 
-namespace BitePaper.Api.Controllers.Logs
-{
-    public class CreateLogEndpoint(IMediator mediator) : Endpoint<CreateLogRequest>
-    {
-        public override void Configure()
-        {
-            Post("log/create");
-            AllowAnonymous();
-        }
+namespace BitePaper.Api.Controllers.Logs;
 
-        public override async Task HandleAsync(CreateLogRequest request, CancellationToken cancellationToken)
-        {
-            await mediator.Send(new CreateLogCommand(request), cancellationToken);
-            await SendNoContentAsync(cancellationToken);
-        }
+public class CreateLogEndpoint(IMediator mediator) : Endpoint<CreateLogRequest>
+{
+    public override void Configure()
+    {
+        Post("log/create");
+        AllowAnonymous();
+    }
+
+    public override async Task HandleAsync(CreateLogRequest request, CancellationToken cancellationToken)
+    {
+        await mediator.Send(new CreateLogCommand(request), cancellationToken);
+        await SendNoContentAsync(cancellationToken);
     }
 }
