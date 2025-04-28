9b306ef711df2feffb801088087bbeb8a1c097c4
Nazarii Soboliuk <109824446+Nryyy@users.noreply.github.com>
2025-04-28 21:20:22 +0300

    Update CreateSignatureEndpoint.cs
    new namespace

--- backend-bitepaper/BitePaper.Api/Controllers/Signatures/CreateSignatureEndpoint.cs
+++ backend-bitepaper/BitePaper.Api/Controllers/Signatures/CreateSignatureEndpoint.cs
@@ -3,20 +3,19 @@ using BitePaper.Models.DTOs.Request.Signatures;
 using FastEndpoints;
 using MediatR;
 
-namespace BitePaper.Api.Controllers.Signatures
-{
-    public class CreateSignatureEndpoint(IMediator mediator) : Endpoint<CreateSignatureRequest>
-    {
-        public override void Configure()
-        {
-            Post("signature/create");
-            AllowAnonymous();
-        }
+namespace BitePaper.Api.Controllers.Signatures;
 
-        public override async Task HandleAsync(CreateSignatureRequest request, CancellationToken cancellationToken)
-        {
-            await mediator.Send(new CreateSignatureCommand(request), cancellationToken);
-            await SendNoContentAsync(cancellationToken);
-        }
+public class CreateSignatureEndpoint(IMediator mediator) : Endpoint<CreateSignatureRequest>
+{
+    public override void Configure()
+    {
+        Post("signature/create");
+        AllowAnonymous();
+    }
+
+    public override async Task HandleAsync(CreateSignatureRequest request, CancellationToken cancellationToken)
+    {
+        await mediator.Send(new CreateSignatureCommand(request), cancellationToken);
+        await SendNoContentAsync(cancellationToken);
     }
 }
