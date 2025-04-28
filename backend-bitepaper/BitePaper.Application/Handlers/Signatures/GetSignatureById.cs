3273d99658f153c6f7bddc79e23d16424a0a04a8
Nazarii Soboliuk <109824446+Nryyy@users.noreply.github.com>
2025-04-28 21:30:50 +0300

    Update GetSignatureById.cs
    new namespace

--- backend-bitepaper/BitePaper.Application/Handlers/Signatures/GetSignatureById.cs
+++ backend-bitepaper/BitePaper.Application/Handlers/Signatures/GetSignatureById.cs
@@ -3,15 +3,14 @@ using BitePaper.Infrastructure.Interfaces.Signatures;
 using BitePaper.Models.Entities;
 using MediatR;
 
-namespace BitePaper.Application.Handlers.Signatures
+namespace BitePaper.Application.Handlers.Signatures;
+
+public class GetSignatureById : IRequestHandler<GetSignatureByIdQuery, Signature?>
 {
-    public class GetSignatureById : IRequestHandler<GetSignatureByIdQuery, Signature?>
+    private readonly ISignatureService _signatureService;
+    public GetSignatureById(ISignatureService signatureService)
     {
-        private readonly ISignatureService _signatureService;
-        public GetSignatureById(ISignatureService signatureService)
-        {
-            _signatureService = signatureService;
-        }
-        public async Task<Signature?> Handle(GetSignatureByIdQuery request, CancellationToken cancellationToken) => await _signatureService.GetByIdAsync(request.id);
+        _signatureService = signatureService;
     }
+    public async Task<Signature?> Handle(GetSignatureByIdQuery request, CancellationToken cancellationToken) => await _signatureService.GetByIdAsync(request.id);
 }
