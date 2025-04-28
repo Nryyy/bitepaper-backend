bd758adc790f20dab8c8b8683c435a363b08c9a9
Nazarii Soboliuk <109824446+Nryyy@users.noreply.github.com>
2025-04-28 21:18:14 +0300

    Update DeleteLogEndpoint.cs
    new namespace

--- backend-bitepaper/BitePaper.Api/Controllers/Logging/DeleteLogEndpoint.cs
+++ backend-bitepaper/BitePaper.Api/Controllers/Logging/DeleteLogEndpoint.cs
@@ -3,19 +3,18 @@ using BitePaper.Models.DTOs.Request.Logs;
 using FastEndpoints;
 using MediatR;
 
-namespace BitePaper.Api.Controllers.Logs
+namespace BitePaper.Api.Controllers.Logs;
+
+public class DeleteLogEndpoint(IMediator mediator) : Endpoint<DeleteLogRequest>
 {
-    public class DeleteLogEndpoint(IMediator mediator) : Endpoint<DeleteLogRequest>
+    public override void Configure()
     {
-        public override void Configure()
-        {
-            Delete("log/delete/{id}");
-            AllowAnonymous();
-        }
-        public override async Task HandleAsync(DeleteLogRequest request, CancellationToken ct)
-        {
-            await mediator.Send(new DeleteLogCommand(request.Id), ct);
-            await SendAsync("Delete succeed!", cancellation: ct);
-        }
+        Delete("log/delete/{id}");
+        AllowAnonymous();
+    }
+    public override async Task HandleAsync(DeleteLogRequest request, CancellationToken ct)
+    {
+        await mediator.Send(new DeleteLogCommand(request.Id), ct);
+        await SendAsync("Delete succeed!", cancellation: ct);
     }
 }
