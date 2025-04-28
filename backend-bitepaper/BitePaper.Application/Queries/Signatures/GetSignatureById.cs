18f08ac0da6184d17ebd40d2753bd5f93e54f7f0
Nazarii Soboliuk <109824446+Nryyy@users.noreply.github.com>
2025-04-28 21:34:57 +0300

    Update GetSignatureById.cs
    new namespace

--- backend-bitepaper/BitePaper.Application/Queries/Signatures/GetSignatureById.cs
+++ backend-bitepaper/BitePaper.Application/Queries/Signatures/GetSignatureById.cs
@@ -1,7 +1,6 @@
 ﻿using BitePaper.Models.Entities;
 using MediatR;
 
-namespace BitePaper.Application.Queries.Signatures
-{
-    public record GetSignatureByIdQuery(string id) : IRequest<Signature?>;
-}
+namespace BitePaper.Application.Queries.Signatures;
+
+public record GetSignatureByIdQuery(string id) : IRequest<Signature?>;
