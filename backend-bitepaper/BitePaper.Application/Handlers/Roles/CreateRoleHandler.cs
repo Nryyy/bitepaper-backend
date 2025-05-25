04b4a1dd65d5186457fcf957cd1ad844b9f33b13
Unding <samanta.verdoes@gmail.com>
2025-05-26 02:24:48 +0300

    Create role fix

--- backend-bitepaper/BitePaper.Application/Handlers/Roles/CreateRoleHandler.cs
+++ backend-bitepaper/BitePaper.Application/Handlers/Roles/CreateRoleHandler.cs
@@ -1,10 +1,22 @@
 ﻿using BitePaper.Application.Commands.Roles;
 using BitePaper.Infrastructure.Interfaces.Roles;
+using BitePaper.Models.Entities;
 using MediatR;
 
 namespace BitePaper.Application.Handlers.Roles;
-public class CreateRoleHandler(IRoleService roleService) : IRequestHandler<CreateRoleCommand>
+public class CreateRoleHandler : IRequestHandler<CreateRoleCommand>
 {
-    public async Task Handle(CreateRoleCommand request, CancellationToken cancellationToken) =>
-        await roleService.CreateAsync(request.Role);
+    private readonly IRoleService _roleService;
+    public CreateRoleHandler(IRoleService roleService)
+    {
+        _roleService = roleService;
+    }
+    public async Task Handle(CreateRoleCommand request, CancellationToken cancellationToken)
+    {
+        var role = new Role
+        {
+            Name = request.Role.Name,
+        };
+        await _roleService.CreateAsync(role);
+    }
 }
